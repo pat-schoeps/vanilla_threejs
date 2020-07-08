196685e6499e539219d5032efb0a03d42d001b29
ljil;kj <schoeps.pat@gmail.com>
2020-07-07 19:31:28 -0700

    stash

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { Scene } from './scene/main';
+import './stylesheets/index.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const scene = new Scene();
+  //requestAnimationFrame(scene.render);
+});
